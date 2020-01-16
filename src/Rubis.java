fc17a41db6becff5d0a5db1b10b73fb2d924b659
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2020-01-16 16:00:38 +0100

    Update Rubis.java

--- src/Rubis.java
+++ src/Rubis.java
@@ -1,28 +1,6 @@
-public class Rubis {              //Un rubis vaut 2 dans le jeu
-    private int x;
-    private int y;
-    private String nom;
+public class Rubis extends Tuile {      //Un rubis vaut 2 dans le jeu
 
-    public Rubis(String nom, int x, int y){
-        this.nom = nom;
-        this.x = x;
-        this.y = y;
+    public Rubis(String typeDeTuile, int x, int y) {
+        super(typeDeTuile, x, y);
     }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
 }
