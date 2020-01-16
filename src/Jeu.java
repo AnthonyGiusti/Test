43ad985630dd175e6d96b7ec3ef29303ed2ab38a
AnthonyGiusti <58392616+AnthonyGiusti@users.noreply.github.com>
2020-01-16 16:04:05 +0100

    Update Jeu.java

--- src/Jeu.java
+++ src/Jeu.java
@@ -21,7 +21,7 @@ public class Jeu {
 
         boolean jeu = true;
 
-        while (jeu == true) {
+        while (jeu) {
 
             System.out.println("Choisir le nombre joueur");
             int nb_joueurs = scanner.nextInt();
@@ -40,9 +40,9 @@ public class Jeu {
                 }
 
                 if (nb_joueurs == 2) {
-                    tortue1 = new Tortue("orange", 0, 1);
-                    tortue2 = new Tortue("bleu", 0, 5);
-                    murPierre = new Mur("Mur", 0, 7);
+                    tortue1 = new Tortue("orange", 0, 1, "sud");
+                    tortue2 = new Tortue("bleu", 0, 5, "sud");
+                    murPierre = new Mur("Mur", 0, 7, "pierre");
                     rubis1 = new Rubis("rubis1", 7, 3);
 
                     plateau[tortue1.getY()][tortue1.getX()] = 1;
@@ -54,21 +54,21 @@ public class Jeu {
 
                     plateau[rubis1.getY()][rubis1.getX()] = 2;
 
-
-                    for (int colonne = 0; colonne < 8; colonne++) {
+                    afficherPlateau();
+                    /*for (int colonne = 0; colonne < 8; colonne++) {
                         for (int ligne = 0; ligne < 8; ligne++) {
                             System.out.print(plateau[ligne][colonne] + "\t"); // On affiche le plateau
                             if (ligne == 7) {
                                 System.out.println("\n");
                             }
                         }
-                    }
+                    }*/
                 }
                 else if (nb_joueurs == 3) {
-                    tortue1 = new Tortue("orange", 0, 0);
-                    tortue2 = new Tortue("bleu", 0, 3);
-                    tortue3 = new Tortue("violet", 0, 6);
-                    murPierre = new Mur("Mur", 0, 7);
+                    tortue1 = new Tortue("orange", 0, 0, "sud");
+                    tortue2 = new Tortue("bleu", 0, 3, "sud");
+                    tortue3 = new Tortue("violet", 0, 6, "sud");
+                    murPierre = new Mur("Mur", 0, 7, "pierre");
                     rubis1 = new Rubis("rubis1", 7, 0);
                     rubis2 = new Rubis("rubis2", 7, 3);
                     rubis3 = new Rubis("rubis1", 7, 6);
@@ -85,20 +85,20 @@ public class Jeu {
                     plateau[rubis2.getY()][rubis2.getX()] = 2;
                     plateau[rubis3.getY()][rubis3.getX()] = 2;
 
-
-                    for (int colonne = 0; colonne < 8; colonne++) {
+                    afficherPlateau();
+                    /*for (int colonne = 0; colonne < 8; colonne++) {
                         for (int ligne = 0; ligne < 8; ligne++) {
                             System.out.print(plateau[ligne][colonne] + "\t"); // On affiche le plateau
                             if (ligne == 7) {
                                 System.out.println("\n");
                             }
                         }
-                    }
+                    }*/
                 } else {
-                    tortue1 = new Tortue("orange", 0, 0);
-                    tortue2 = new Tortue("bleu", 0, 2);
-                    tortue3 = new Tortue("violet", 0, 5);
-                    tortue4 = new Tortue("jaune", 0, 7);
+                    tortue1 = new Tortue("orange", 0, 0, "sud");
+                    tortue2 = new Tortue("bleu", 0, 2, "sud");
+                    tortue3 = new Tortue("violet", 0, 5, "sud");
+                    tortue4 = new Tortue("jaune", 0, 7, "sud");
                     rubis1 = new Rubis("rubis1", 7, 1);
                     rubis2 = new Rubis("rubis2", 7, 6);
 
@@ -110,27 +110,35 @@ public class Jeu {
                     plateau[rubis1.getY()][rubis1.getX()] = 2;
                     plateau[rubis2.getY()][rubis2.getX()] = 2;
 
-                    for (int colonne = 0; colonne < 8; colonne++) {
+                    afficherPlateau();
+                    /*for (int colonne = 0; colonne < 8; colonne++) {
                         for (int ligne = 0; ligne < 8; ligne++) {
                             System.out.print(plateau[ligne][colonne] + "\t"); // On affiche le plateau
                             if (ligne == 7) {
                                 System.out.println("\n");
                             }
                         }
-                    }
+                    }*/
                 }
-                System.out.println("Que voulez-vous faire ?\n 1.Compléter le programme\n 2.Construire un mur\n 3.Exéctuer le programme");
-                int choix = scanner.nextInt();
+                Joueur joueur = new Joueur();
+            }
+        }
+    }
 
-                switch (choix) {
-                    case 1:
-                        Joueur joueur = new Joueur();
-                    case 2:
-
-                    case 3:
-
+    public void afficherPlateau(){
+        for (int colonne = 0; colonne < 8; colonne++) {
+            for (int ligne = 0; ligne < 8; ligne++) {
+                System.out.print(plateau[ligne][colonne] + "\t"); // On affiche le plateau
+                if (ligne == 7) {
+                    System.out.println("\n");
                 }
             }
         }
+    }
+    public static void deplacement(int numeroTortue, int y){
+        if (numeroTortue == 1){
+            //tortue1.setY(y);
+        }
+
     }
 }
